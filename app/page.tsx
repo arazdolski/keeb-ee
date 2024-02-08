610dc2cf6c3049a2900cf4f24d92e23a71ee723b
Valmar <valmarparts@gmail.com>
2024-02-08 19:54:02 +0200

    Update layout text

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,23 @@ export default function Home() {
     <main>
       <header>
         <Navbar></Navbar>
-        <p className="pt-8 pb-2 pl-2 text-left sm:text-center text-lg sm:text-xl md:text-2xl lg:text-3xl">
-          Our community revolves around a passion for mechanical keyboards.{" "}
-          <br />
-          We build, share, design keebs, but it's mostly to make friends and
-          goof around. <br />
-          Each of us has a unique vision for how a keyboard should look and
-          feel.
-          <br />
-          <br />
-          Sending love from chilly Estonia!
-        </p>
+        <div className="relative flex items-center justify-center md:h-screen h-auto flex-col md:flex-row md:pt-0 pt-4">
+          <div className="md:absolute  h-auto flex items-center justify-start">
+            <div className="text-left  md:w-1/2 m-auto">
+              <p className="pt-8 pb-2 pl-2 text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+                Our community revolves around a passion for mechanical
+                keyboards. <br />
+                We build, share, design keebs, but it's mostly to make friends
+                and goof around. <br />
+                Each of us has a unique vision for how a keyboard should look
+                and feel.
+                <br />
+                <br />
+                Sending love from chilly Estonia!
+              </p>
+            </div>
+          </div>
+        </div>
         <Section heading="Upcoming event">
           <Image
             src="/Keebuary-banner.png"
