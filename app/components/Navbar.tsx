92d9105241dd066430e2eeade9944ebf84ce5871
Valmar <valmarparts@gmail.com>
2024-02-08 14:56:39 +0200

    Add fonts, images, styling and update layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,25 +1,21 @@
-"use client";
-import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Navbar: React.FC = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
-
   return (
     <nav>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto sm:px-4 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex sm:justify-center w-full">
             {/* Logo */}
             <div className="flex-shrink-0 flex items-center align-left sm:absolute sm:left-1/2 sm:-translate-x-1/2">
               <Link href="/">
                 <div className="pt-2 flex flex-col sm:flex-row items-start sm:items-center">
-                  <p className="pt-4 pl-2 sm:pt-0 sm:pr-1 text-left">
+                  <p className="pt-4 pl-2 sm:pt-0 sm:pr-1 text-sm sm:text-base lg:text-2xl">
                     Welcome to
                   </p>
                   <Image
-                    src="/keeb-logo-horizontal-void.svg"
+                    src="logos/keeb-logo-horizontal-void.svg"
                     width={250}
                     height={250}
                     alt="Picture of the author"
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,45 @@
-import Image from 'next/image';
-import Navbar from './components/Navbar';
-import Section from './components/Section';
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
 
 export default function Home() {
   return (
     <main>
       <header>
         <Navbar></Navbar>
-        <p>
-          Our community revolves around a passion for mechanical keyboards.{' '}
+        <p className="pt-8 pb-2 pl-2 text-left sm:text-center text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          Our community revolves around a passion for mechanical keyboards.{" "}
           <br />
           We build, share, design keebs, but it's mostly to make friends and
           goof around. <br />
           Each of us has a unique vision for how a keyboard should look and
           feel.
           <br />
+          <br />
           Sending love from chilly Estonia!
         </p>
-        <Section
-          heading='Upcoming event'
-          logo='/illustration-CLAP.svg'
-          logoPosition='left'
-        >
+        <Section heading="Upcoming event">
           <Image
-            src='/Keebuary-banner.png'
+            src="/Keebuary-banner.png"
             width={1200}
             height={800}
-            alt='Picture of the author'
+            alt="Banner image of the upcoming keeb.ee event"
           />
         </Section>
-        <Section heading='Keebs we have built'>
+        <Section heading="Keebs we have built">
           <Image
-            src='/Keebuary-banner.png'
+            src="/Keebuary-banner.png"
             width={1200}
             height={800}
-            alt='Picture of the author'
+            alt="Picture of the author"
           />
         </Section>
-        <Section heading='Meet-ups we have had'>
+        <Section heading="Meet-ups we have had">
           <Image
-            src='/Keebuary-banner.png'
+            src="/Keebuary-banner.png"
             width={1200}
             height={800}
-            alt='Picture of the author'
+            alt="Picture of the author"
           />
         </Section>
       </header>
