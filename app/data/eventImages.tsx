c59f76231e48b55f60369033f44a87d4adc2bb8a
valmar parts <valmarparts@gmail.com>
2024-03-03 14:24:41 +0200

    Update images, logos and banner

--- app/data/eventImages.tsx
+++ app/data/eventImages.tsx
@@ -1,90 +1,172 @@
 const eventImages: { src: string; alt: string }[] = [
   {
-    src: '/Artboard 20.png',
+    src: '/springswap/Artboard 20.png',
     alt: 'From our third meetup, people looking at keyboards',
   },
   {
-    src: '/Artboard 21.png',
+    src: '/springswap/Artboard 21.png',
     alt: 'From our third meetup, people looking at keyboards',
   },
-  { src: '/Artboard 22.png', alt: 'Third meetup close up of Ginko65 keyboard' },
   {
-    src: '/Artboard 23.png',
+    src: '/springswap/Artboard 22.png',
+    alt: 'Third meetup close up of Ginko65 keyboard',
+  },
+  {
+    src: '/springswap/Artboard 23.png',
     alt: 'Third meetup soldering workshop, soldering the pcb',
   },
   {
-    src: '/Artboard 24.png',
+    src: '/springswap/Artboard 24.png',
     alt: 'Third meetup soldering workshop, close up of soldering of the pcb',
   },
   {
-    src: '/Artboard 25.png',
+    src: '/springswap/Artboard 25.png',
     alt: 'Third meetup soldering workshop, close up of the keyboard case',
   },
   {
-    src: '/Artboard 26.png',
+    src: '/springswap/Artboard 26.png',
     alt: 'Third meetup, close up of touching the 40% ergo keyboard',
   },
   {
-    src: '/Artboard 27.png',
+    src: '/springswap/Artboard 27.png',
     alt: 'Third meetup, close up of touching the alice keyboard',
   },
   {
-    src: '/Artboard 28.png',
+    src: '/springswap/Artboard 28.png',
     alt: 'Third meetup, people around keyboard table',
   },
   {
-    src: '/Artboard 29.png',
+    src: '/springswap/Artboard 29.png',
     alt: 'Third meetup, people positioning the keyboards on the table',
   },
   {
-    src: '/Artboard 30.png',
+    src: '/firstmeetup/Artboard 30.png',
     alt: 'First meetup, picture of all of the keyboards present',
   },
-  { src: '/Artboard 31.png', alt: 'First meetup, setting up the keyboards' },
   {
-    src: '/Artboard 32.png',
+    src: '/firstmeetup/Artboard 31.png',
+    alt: 'First meetup, setting up the keyboards',
+  },
+  {
+    src: '/firstmeetup/Artboard 32.png',
     alt: 'First meetup, over shot image of the keyboards and their labels',
   },
   {
-    src: '/Artboard 33.png',
+    src: '/springswap/Artboard 33.png',
     alt: 'Third meetup, picture of the people in the soldering workshop',
   },
   {
-    src: '/Artboard 34.png',
-    alt: 'Third meetup, soldering workshop macropad building',
-  },
-  {
-    src: '/Artboard 35.png',
-    alt: 'Third meetup, adjusting the solder of the pcb',
-  },
-  {
-    src: '/Artboard 36.png',
+    src: '/springswap/Artboard 36.png',
     alt: 'Third meetup, collection of different keyboards on the table',
   },
+  { src: '/springswap/Artboard 39.png', alt: 'Third meetup, close up of QK60' },
   {
-    src: '/Artboard 37.png',
-    alt: 'Third meetup, man thinking in the soldering workshop',
-  },
-  {
-    src: '/Artboard 38.png',
-    alt: 'Third meetup, people preparing for the soldering workshop',
-  },
-  { src: '/Artboard 39.png', alt: 'Third meetup, close up of QK60' },
-  {
-    src: '/Artboard 40.png',
+    src: '/springswap/Artboard 40.png',
     alt: 'Third meetup, close up of a Cocot keyboard with trackball',
   },
   {
-    src: '/Artboard 41.png',
+    src: '/springswap/Artboard 41.png',
     alt: 'Third meetup, overview of the keyboards on the table',
   },
   {
-    src: '/Artboard 42.png',
+    src: '/firstmeetup/Artboard 42.png',
     alt: 'First meetup, overview of the keyboards and trying out keyboards',
   },
   {
-    src: '/Artboard 43.png',
+    src: '/firstmeetup/Artboard 43.png',
     alt: 'First meetup, overview of the keyboards from a different angle',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-1.jpg',
+    alt: 'People gathering and looking at keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-2.jpg',
+    alt: 'People viewing differend displayed keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-3.jpg',
+    alt: 'Person inspecting a keyboard up close.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-4.jpg',
+    alt: 'Three people looking at a keyboard.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-5.jpg',
+    alt: 'People inspecting and holding keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-6.jpg',
+    alt: 'Keyboards displayed on a table, people gathered around them.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-7.jpg',
+    alt: 'Upclose shot of keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-8.jpg',
+    alt: 'People discussing kewyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-10.jpg',
+    alt: 'People inspecting keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-11.jpg',
+    alt: 'Person writing description of the keyboard build.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-12.jpg',
+    alt: 'Two people discussing split keyboard build.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-15.jpg',
+    alt: 'Picture of keyboards and with descriptions.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-16.jpg',
+    alt: 'Upclose shot of keyboards, people in the background.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-17.jpg',
+    alt: 'People trying out a keyboard.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-18.jpg',
+    alt: 'Keybords and people around them.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-19.jpg',
+    alt: 'Selfie of a group of people with keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-20.jpg',
+    alt: 'People interacting with each other and keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-21.jpg',
+    alt: 'A picture of a keyboard vest.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-22.jpg',
+    alt: 'Person trying on the keyboard vest.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-23.jpg',
+    alt: 'People listening to a presentation.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-25.jpg',
+    alt: 'Overview of keyboards.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-26.jpg',
+    alt: 'Person giving a presentation, people listening.',
+  },
+  {
+    src: '/keebuary/Keebuary-EVENT-27.jpg',
+    alt: 'Beer can design of anderson and keeb collaboration.',
   },
 ];
 
