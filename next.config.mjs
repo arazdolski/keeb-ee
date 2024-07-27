7f1428b9c6e52a323f240e091f70401c1401de15
arazdolski <aleksandr.razdolski@gmail.com>
2024-07-27 12:05:51 +0300

    Update caniuse-lite, replace domains with remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['keeb.ee'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'keeb.ee',
+        port: '',
+      },
+    ],
   },
 };
 
